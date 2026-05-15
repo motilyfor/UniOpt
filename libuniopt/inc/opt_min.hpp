8ec373598e47907c3d4c820bb3a03103bc2e10f2
motilyfor <motilfyor@foxmail.com>
2026-05-16 00:41:17 +0800

    新增：实现整数加法、减法、最大值和最小值的向量化操作，支持NEON和AVX2优化

---
+++ libuniopt/inc/opt_min.hpp
@@ -0,0 +1,132 @@
+#pragma once
+
+#include "opt_base.hpp"
+#include <cstdint>
+
+#if defined(OPT_AVX2)
+#    include <immintrin.h>
+#elif defined(OPT_NEON)
+#    include <arm_neon.h>
+#endif
+
+INLINE int16_t opt_min_i16(int16_t x, int16_t y)
+{
+    return (x < y) ? x : y;
+}
+
+INLINE int32_t opt_min_i32(int32_t x, int32_t y)
+{
+    return (x < y) ? x : y;
+}
+
+INLINE void opt_vec_min_i16(int16_t* RESTRICT dst, const int16_t* RESTRICT src,
+                            const int16_t N, std::size_t count)
+{
+#if defined(OPT_NEON)
+    const int16x8_t limit = vdupq_n_s16(N);
+    std::size_t i = 0;
+    for (; i + 8 <= count; i += 8) {
+        int16x8_t a = vld1q_s16(src + i);
+        vst1q_s16(dst + i, vminq_s16(a, limit));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i16(src[i], N);
+    }
+#elif defined(OPT_AVX2)
+    const __m256i limit = _mm256_set1_epi16(N);
+    std::size_t i = 0;
+    for (; i + 16 <= count; i += 16) {
+        __m256i a = _mm256_loadu_si256((const __m256i*)(src + i));
+        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_min_epi16(a, limit));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i16(src[i], N);
+    }
+#else
+    for (std::size_t i = 0; i < count; ++i) dst[i] = opt_min_i16(src[i], N);
+#endif
+}
+
+INLINE void opt_vec_min_i32(int32_t* RESTRICT dst, const int32_t* RESTRICT src,
+                            const int32_t N, std::size_t count)
+{
+#if defined(OPT_NEON)
+    const int32x4_t limit = vdupq_n_s32(N);
+    std::size_t i = 0;
+    for (; i + 4 <= count; i += 4) {
+        int32x4_t a = vld1q_s32(src + i);
+        vst1q_s32(dst + i, vminq_s32(a, limit));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i32(src[i], N);
+    }
+#elif defined(OPT_AVX2)
+    const __m256i limit = _mm256_set1_epi32(N);
+    std::size_t i = 0;
+    for (; i + 8 <= count; i += 8) {
+        __m256i a = _mm256_loadu_si256((const __m256i*)(src + i));
+        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_min_epi32(a, limit));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i32(src[i], N);
+    }
+#else
+    for (std::size_t i = 0; i < count; ++i) dst[i] = opt_min_i32(src[i], N);
+#endif
+}
+
+INLINE void opt_vec_min_i16(int16_t* RESTRICT dst, const int16_t* RESTRICT src,
+                            const int16_t* RESTRICT limit, std::size_t count)
+{
+#if defined(OPT_NEON)
+    std::size_t i = 0;
+    for (; i + 8 <= count; i += 8) {
+        int16x8_t a = vld1q_s16(src + i);
+        int16x8_t b = vld1q_s16(limit + i);
+        vst1q_s16(dst + i, vminq_s16(a, b));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i16(src[i], limit[i]);
+    }
+#elif defined(OPT_AVX2)
+    std::size_t i = 0;
+    for (; i + 16 <= count; i += 16) {
+        __m256i a = _mm256_loadu_si256((const __m256i*)(src + i));
+        __m256i b = _mm256_loadu_si256((const __m256i*)(limit + i));
+        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_min_epi16(a, b));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i16(src[i], limit[i]);
+    }
+#else
+    for (std::size_t i = 0; i < count; ++i) dst[i] = opt_min_i16(src[i], limit[i]);
+#endif
+}
+
+INLINE void opt_vec_min_i32(int32_t* RESTRICT dst, const int32_t* RESTRICT src,
+                            const int32_t* RESTRICT limit, std::size_t count)
+{
+#if defined(OPT_NEON)
+    std::size_t i = 0;
+    for (; i + 4 <= count; i += 4) {
+        int32x4_t a = vld1q_s32(src + i);
+        int32x4_t b = vld1q_s32(limit + i);
+        vst1q_s32(dst + i, vminq_s32(a, b));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i32(src[i], limit[i]);
+    }
+#elif defined(OPT_AVX2)
+    std::size_t i = 0;
+    for (; i + 8 <= count; i += 8) {
+        __m256i a = _mm256_loadu_si256((const __m256i*)(src + i));
+        __m256i b = _mm256_loadu_si256((const __m256i*)(limit + i));
+        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_min_epi32(a, b));
+    }
+    for (; i < count; ++i) {
+        dst[i] = opt_min_i32(src[i], limit[i]);
+    }
+#else
+    for (std::size_t i = 0; i < count; ++i) dst[i] = opt_min_i32(src[i], limit[i]);
+#endif
+}
--- libuniopt/inc/opt_sub.hpp
+++ libuniopt/inc/opt_sub.hpp
@@ -4,81 +4,79 @@
 #include <cstdint>
 #include <climits>
 
-
-INLINE int16_t opt_add_i16(int16_t x, const int16_t y)
+INLINE int16_t opt_sub_i16(int16_t x, const int16_t y)
 {
     int16_t res;
-    if (__builtin_add_overflow(x, y, &res)) {
-        // Overflow only occurs when x and y share the same sign.
+    if (__builtin_sub_overflow(x, y, &res)) {
+        // Overflow only occurs when x and y have different signs.
         return static_cast<int16_t>((static_cast<int32_t>(x) >> 15) ^ INT16_MAX);
     }
     return res;
 }
 
-INLINE int32_t opt_add_i32(int32_t x, const int32_t y)
-{
+INLINE int32_t opt_sub_i32(int32_t x, const int32_t y)
+{                   
     int32_t res;
-    if (__builtin_add_overflow(x, y, &res)) {
-        // Overflow only occurs when x and y share the same sign.
+    if (__builtin_sub_overflow(x, y, &res)) {
+        // Overflow only occurs when x and y have different signs.
         return (x >> 31) ^ INT32_MAX;
     }
     return res;
 }
 
-INLINE void opt_vec_add_i16(int16_t* RESTRICT dst, const int16_t* RESTRICT x, const int16_t* RESTRICT y, std::size_t count)
+INLINE void opt_vec_sub_i16(int16_t* RESTRICT dst, const int16_t* RESTRICT x, const int16_t* RESTRICT y, std::size_t count)
 {
 #if defined(OPT_NEON)
     std::size_t i = 0;
     for (; i + 8 <= count; i += 8) {
         int16x8_t a = vld1q_s16(x + i);
         int16x8_t b = vld1q_s16(y + i);
-        vst1q_s16(dst + i, vqaddq_s16(a, b));
+        vst1q_s16(dst + i, vqsubq_s16(a, b));
     }
     for (; i < count; ++i) {
-        dst[i] = opt_add_i16(x[i], y[i]);
+        dst[i] = opt_sub_i16(x[i], y[i]);
     }
 #elif defined(OPT_AVX2)
     std::size_t i = 0;
     for (; i + 16 <= count; i += 16) {
         __m256i a = _mm256_loadu_si256((const __m256i*)(x + i));
         __m256i b = _mm256_loadu_si256((const __m256i*)(y + i));
-        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_adds_epi16(a, b));
+        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_subs_epi16(a, b));
     }
     for (; i < count; ++i) {
-        dst[i] = opt_add_i16(x[i], y[i]);
+        dst[i] = opt_sub_i16(x[i], y[i]);
     }
 #else
     for (std::size_t i = 0; i < count; ++i) {
-        dst[i] = opt_add_i16(x[i], y[i]);
+        dst[i] = opt_sub_i16(x[i], y[i]);
     }
 #endif
 }
 
-INLINE void opt_vec_add_i32(int32_t* RESTRICT dst, const int32_t* RESTRICT x, const int32_t* RESTRICT y, std::size_t count)
+INLINE void opt_vec_sub_i32(int32_t* RESTRICT dst, const int32_t* RESTRICT x, const int32_t* RESTRICT y, std::size_t count)
 {
 #if defined(OPT_NEON)
     std::size_t i = 0;
     for (; i + 4 <= count; i += 4) {
         int32x4_t a = vld1q_s32(x + i);
         int32x4_t b = vld1q_s32(y + i);
-        vst1q_s32(dst + i, vqaddq_s32(a, b));
+        vst1q_s32(dst + i, vqsubq_s32(a, b));
     }
     for (; i < count; ++i) {
-        dst[i] = opt_add_i32(x[i], y[i]);
+        dst[i] = opt_sub_i32(x[i], y[i]);
     }
 #elif defined(OPT_AVX2)
     std::size_t i = 0;
     for (; i + 8 <= count; i += 8) {
         __m256i a = _mm256_loadu_si256((const __m256i*)(x + i));
         __m256i b = _mm256_loadu_si256((const __m256i*)(y + i));
-        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_add_epi32(a, b));
+        _mm256_storeu_si256((__m256i*)(dst + i), _mm256_sub_epi32(a, b));
     }
     for (; i < count; ++i) {
-        dst[i] = opt_add_i32(x[i], y[i]);
+        dst[i] = opt_sub_i32(x[i], y[i]);   
     }
 #else
-    for (std::size_t i = 0; i < count; ++i) {
-        dst[i] = opt_add_i32(x[i], y[i]);
-    }
+    for (std::size_t i = 0; i < count; ++i)
+        dst[i] = opt_sub_i32(x[i], y[i]);
 #endif
 }
